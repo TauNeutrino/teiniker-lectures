f0169036bc19ca327c0755a16af3fa49119cf9fa
egon.teiniker <egon.teiniker@1e180149-9672-02dd-e6c9-76f92baa73ab>
2015-05-12 07:15:19 +0000

    Initial import.

---
+++ enterprise-architectures/4_ServiceOrientedArchitectures/REST-UserService/src/main/java/org/se/lab/RESTApplication.java
@@ -0,0 +1,30 @@
+package org.se.lab;
+
+import java.util.Set;
+import java.util.HashSet;
+import javax.ws.rs.core.Application;
+
+public class RESTApplication 
+	extends Application
+{
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> classes = new HashSet<Class<?>>();
+
+	public RESTApplication()
+	{
+//		singletons.add(new UserResourceImpl());
+		classes.add(UserResourceImpl.class);
+	}
+
+	@Override
+	public Set<Class<?>> getClasses()
+	{
+		return classes;
+	}
+
+	@Override
+	public Set<Object> getSingletons()
+	{
+		return singletons;
+	}
+}
