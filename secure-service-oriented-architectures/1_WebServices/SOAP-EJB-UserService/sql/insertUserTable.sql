402d566419d15b98372d6e175e69c827f3216ea4
teiniker <egon.teiniker@gmail.com>
2015-10-01 10:17:28 +0200

    Initial import.

---
+++ secure-service-oriented-architectures/1_WebServices/SOAP-EJB-UserService/sql/insertUserTable.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO user (username, password) VALUES ('homer', '**********');
+INSERT INTO user (username, password) VALUES ('marge', '**********');
+INSERT INTO user (username, password) VALUES ('bart', '**********');
+INSERT INTO user (username, password) VALUES ('lisa', '**********');
+
+
