e8a35f8ddb94df5c65e101a7732ef1b41622f43c
teiniker <egon.teiniker@gmail.com>
2015-10-17 15:36:56 +0200

    Added Service Conector.

--- secure-service-oriented-architectures/1_WebServices/REST-EJB-UserService-Client/src/test/java/org/se/lab/service/UserServiceJSONTest.java
+++ secure-service-oriented-architectures/1_WebServices/REST-EJB-UserService-Client/src/test/java/org/se/lab/service/UserServiceJSONTest.java
@@ -34,6 +34,7 @@ public class UserServiceJSONTest
 	public void testFindById() throws IOException, JAXBException
 	{
 		URL url = new URL("http://" + HOST + ":" + PORT + WEB_APP_NAME + "/users/3");
+		System.out.println(url.toExternalForm());
 		
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection(PROXY);
 		connection.setRequestMethod("GET");
@@ -42,7 +43,7 @@ public class UserServiceJSONTest
 		int httpResponseCode = connection.getResponseCode();
 		Assert.assertEquals(200, httpResponseCode);
 		String content = readResponseContent(connection.getInputStream());	
-		final String EXPECTED = "{\"id\":3,\"username\":\"bart\",\"password\":\"**********\"}\n";		
+		final String EXPECTED = "{\"id\":3,\"username\":\"bart\",\"password\":\"Ls4jKY8G2ftFdy/bHTgIaRjID0Q=\"}\n";		
 		System.out.println("Response-Content:\n" + content);
 		Assert.assertEquals(EXPECTED, content);
 	}
@@ -64,10 +65,10 @@ public class UserServiceJSONTest
 		
 		final String EXPECTED = 
 				"[" +
-					"{\"id\":1,\"username\":\"homer\",\"password\":\"**********\"}," +
-					"{\"id\":2,\"username\":\"marge\",\"password\":\"**********\"}," +
-					"{\"id\":3,\"username\":\"bart\",\"password\":\"**********\"}," +
-					"{\"id\":4,\"username\":\"lisa\",\"password\":\"**********\"}" +
+					"{\"id\":1,\"username\":\"homer\",\"password\":\"ijD8qepbRnIsva0kx0cKRCcYysg=\"}," +
+					"{\"id\":2,\"username\":\"marge\",\"password\":\"xCSuPDv2U6I5jEO1wqvEQ/jPYhY=\"}," +
+					"{\"id\":3,\"username\":\"bart\",\"password\":\"Ls4jKY8G2ftFdy/bHTgIaRjID0Q=\"}," +
+					"{\"id\":4,\"username\":\"lisa\",\"password\":\"xO0U4gIN1F7bV7X7ovQN2TlSUF4=\"}" +
 				"]\n";
 		System.out.println("Response-Content:\n" + content);
 		Assert.assertEquals(EXPECTED, content);
